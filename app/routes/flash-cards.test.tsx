bfbe506e63c29f813900ab8c01a7f8c7e7f02f5e
Łukasz <lgilski06@gmail.com>
2026-07-25 09:36:32 +0200

    added tests to do

--- app/routes/flash-cards.test.tsx
+++ app/routes/flash-cards.test.tsx
@@ -1,4 +1,12 @@
 import { test } from 'vitest';
 
 // Wyświetlana jest informacja, gdy puste, po kliknięciu wyświetlana jest odpowiedź a po ponownym znów pytanie, nawigacja działa tak jak należy (przyciski) - znam, nie umiem, powtórz niezapamiętane, rozpocznij od nowa
-test.todo('test');
+test.todo('displayes message when empty');
+test.todo('shows answer on click, and question on another click');
+test.todo(
+  'adds cards to repeat and after choosing repeat unlearned it shows only those',
+);
+test.todo('displayes start over when there are no cards to repeat');
+test.todo(
+  'shows the message to edit the cards when there is no card in the group',
+);
