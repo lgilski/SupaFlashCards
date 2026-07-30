39d05f7312aa206f37c480d579444582b67761a9
Łukasz <lgilski06@gmail.com>
2026-07-30 22:12:25 +0200

    Shuffle functionality

--- app/routes/flash-cards.tsx
+++ app/routes/flash-cards.tsx
@@ -4,7 +4,11 @@ import { getServerClient } from '~/utils/supabase.server';
 import type { Route } from './+types/flash-cards';
 import { userContext } from '~/context';
 
-function shuffle(array: any[]) {
+function shuffle(array: any[], shuffle = true) {
+  if (!shuffle) {
+    return array;
+  }
+
   const arrayToShufle = structuredClone(array);
   let currentIndex = arrayToShufle.length;
 
@@ -85,9 +89,10 @@ export function HydrateFallback() {
 
 export default function FlashCards({ loaderData }: Route.ComponentProps) {
   const { data, categoryName } = loaderData;
+  const [triggerShuffle, setTriggerShuffle] = useState(false);
   const [cardsToDisplay, setCardsToDisplay] = useState<
     { question: string; answer: string }[]
-  >(shuffle(data));
+  >(shuffle(data, triggerShuffle));
 
   const [showAnswer, setShowAnswer] = useState(false);
   const [currentCard, setCurrentCard] = useState(0);
@@ -110,15 +115,20 @@ export default function FlashCards({ loaderData }: Route.ComponentProps) {
   }
 
   function repeatCards() {
-    setCardsToDisplay(shuffle(cardsToRepeat));
+    setCardsToDisplay(shuffle(cardsToRepeat, triggerShuffle));
     setCardsToRepeat([]);
     setCurrentCard(0);
   }
 
   function startOver() {
-    setCardsToDisplay(shuffle(data));
+    setCardsToDisplay(shuffle(data, triggerShuffle));
     setCardsToRepeat([]);
     setCurrentCard(0);
+  }
+
+  function shuffleChange() {
+    setTriggerShuffle(prevState => !prevState);
+    startOver();
   }
 
   if (cardsToDisplay.length < 1) {
@@ -149,6 +159,11 @@ export default function FlashCards({ loaderData }: Route.ComponentProps) {
             onClick={repeatCards}
           >
             Repeat unlearned cards
+            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+  <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
+  <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
+  <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
+</svg> */}
           </button>
         )}
         <button
@@ -156,13 +171,16 @@ export default function FlashCards({ loaderData }: Route.ComponentProps) {
           onClick={startOver}
         >
           Start over
+          {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+  <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clipRule="evenodd" />
+</svg> */}
         </button>
       </section>
     );
   }
 
   return (
-    <section className='max-w-3xl mx-auto flex flex-col gap-4 items-center bg-white p-4 mt-16 shadow-md'>
+    <section className='max-w-3xl mx-auto flex flex-col gap-4 items-center bg-white p-4 mt-16 shadow-md relative'>
       <div className='w-full flex gap-2'>
         <h2 className='font-semibold'>{categoryName}</h2>
         <Form action='edit'>
@@ -190,8 +208,23 @@ export default function FlashCards({ loaderData }: Route.ComponentProps) {
           ? `Answer: ${cardsToDisplay[currentCard].answer}`
           : cardsToDisplay[currentCard].question}
       </div>
-      <div>
-        {currentCard + 1}/{cardsToDisplay.length} cards
+      <div className=''>
+        <div>
+          {currentCard + 1}/{cardsToDisplay.length} cards
+        </div>
+        <svg
+          onClick={shuffleChange}
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 24 24'
+          fill='currentColor'
+          className={`size-9 p-1 absolute right-6 -translate-y-7 rounded-full  ${triggerShuffle ? 'text-teal-100 bg-teal-600 hover:bg-teal-500' : 'text-blue-grey-600 bg-blue-grey-050 hover:bg-blue-grey-100'}`}
+        >
+          <path
+            fillRule='evenodd'
+            d='M15.97 2.47a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 1 1-1.06-1.06l3.22-3.22H7.5a.75.75 0 0 1 0-1.5h11.69l-3.22-3.22a.75.75 0 0 1 0-1.06Zm-7.94 9a.75.75 0 0 1 0 1.06l-3.22 3.22H16.5a.75.75 0 0 1 0 1.5H4.81l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 0Z'
+            clipRule='evenodd'
+          />
+        </svg>
       </div>
       <div className='grid grid-cols-2 gap-8'>
         <button
